c319fc4e6e85fb358213a5bd8ca98568757ea5da
Priti Shaw <pritishaw0103@gmail.com>
2020-08-24 12:21:03 +0530

    Fix config file name

--- startup.sh
+++ startup.sh
@@ -14,7 +14,7 @@ papermill Reactome_Failed_Query_Analysis.ipynb failed_query_analysis.ipynb --log
 papermill Reactome_PMID_Metadata_Extraction.ipynb pmid_metadata.ipynb --log-output -f $1
 
 echo "Step 2/2: Running R Notebook"
-cp $1 ./parameters_sample.yml
+cp $1 ./parameters.yml
 R -e "rmarkdown::render('Reactome_Analysis.Rmd',output_file='analysis_output.html')"
 
 echo "Output file : analysis_output.nb.html"
