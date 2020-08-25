97e29cb0f79ac9423089ec8025fe94eb1051015b
PritiShaw <pritishaw0103@gmail.com>
2020-08-25 14:03:29 +0530

    Add Instructions in README

--- startup.sh
+++ startup.sh
@@ -10,12 +10,11 @@ if [ ! -f $1 ]; then
 fi
 
 echo "Step 1/2: Running Python notebooks"
-papermill Reactome_Failed_Query_Analysis.ipynb failed_query_analysis.ipynb --log-output -f $1
-papermill Reactome_PMID_Metadata_Extraction.ipynb pmid_metadata.ipynb --log-output -f $1
+papermill Reactome_Failed_Query_Analysis.ipynb failed_query_analysis.ipynb --log-output -k python3 -f $1
+papermill Reactome_PMID_Metadata_Extraction.ipynb pmid_metadata.ipynb --log-output -k python3 -f $1
 
 echo "Step 2/2: Running R Notebook"
 cp $1 ./parameters.yml
 R -e "rmarkdown::render('Reactome_Analysis.Rmd',output_file='analysis_output.html')"
 
-echo "Output file : analysis_output.nb.html"
 echo "DONE"
