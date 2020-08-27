1a54313bf1f7a8a1563a0aae7e417122d44cacf4
PritiShaw <pritishaw0103@gmail.com>
2020-08-28 03:54:21 +0530

    Add Pyjnus Unit tests

--- reactome_query_utils/tests/test_getEUtilsInfo.py
+++ reactome_query_utils/tests/test_getEUtilsInfo.py
@@ -7,8 +7,7 @@ sys.path.append('../../')
 def test_getEUtilsInfo():
     os.environ["INDRA_DB_REST_URL"] = "https://db.indra.bio"
     pmid_list_path = "pmid_list.txt"
-    with open(pmid_list_path, "w") as f:
-        f.write("32793114~DMRT1~720\n32765585~DMRT1~720\n32752987~DMRT1~720\n32741963~DMRT1~720\n32731992~DMRT1~720\n")
+    
     getEUtilsInfo(pmid_list_path)
     assert os.path.exists("eutils_output.tsv")
 
--- reactome_query_utils/tests/test_getMeSH.py
+++ reactome_query_utils/tests/test_getMeSH.py
@@ -1,12 +1,25 @@
-from reactome_query_utils.getMeSH import getAbstracts
+import jnius_config
+jnius_config.add_classpath("./reactome_query_utils/tests/lib/*")
+from reactome_query_utils.getMeSH import getMeSH
 import sys
 import os
+from jnius import autoclass
+GenericBatchNew = autoclass("GenericBatchNew")
 
 sys.path.append('../../')
 
 
-def test_extractListID():
+def test_getMeSH():
     pmid_path = "pmid_list.txt"
     abstract_path = "abstract.txt"
-    getAbstracts(abstract_path, pmid_path)
-    assert os.path.exists(abstract_path)
+    mesh_output_file = "mesh.txt"
+    username = "PritiShaw"
+    password = "Gsoc2020"
+    processor = GenericBatchNew()
+    result = getMeSH("ci@github.com", username, password, processor,
+                     abstract_filepath=abstract_path,
+                     mesh_output_file=mesh_output_file,
+                     pmid_path=pmid_path
+                    )
+    assert len(result.splitlines()) > 1 and os.path.exists(
+        abstract_path) and os.path.exists(mesh_output_file)
